53754a64e113eff146b4981e69ab0d24f095ad5d
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2022-10-27 13:11:20 +0300

    Stylecop, Logger implementation
    Added a new stylecop file,
    Added Logger implementation

---
+++ Module2Homework1/LogTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Homework1
+{
+    public enum LogTypes
+    {
+        Error,
+        Info,
+        Warning
+    }
+}
---
+++ Module2Homework1/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Homework1
+{
+    public class Result
+    {
+        public Result(bool status, string? message)
+        {
+            Status = status;
+            Message = message;
+        }
+
+        public bool Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
