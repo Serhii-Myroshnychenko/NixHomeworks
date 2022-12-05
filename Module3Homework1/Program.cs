9b028f2b9005b5e29819279191d1165355c53963
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2022-12-05 17:42:10 +0200

    Added starter class

--- Module3Homework1/Program.cs
+++ Module3Homework1/Program.cs
@@ -1,28 +1,3 @@
-﻿using Module3Homework1.Collection;
-using Module3Homework1.Models;
+﻿using Module3Homework1.Starters;
 
-Car[] initArray = new Car[] { new ("AUDI A6", 210), new ("Nissan GTR", 300) };
-Car[] range = new Car[] { new ("BMW 530d", 245), new ("AUDI Q7", 212) };
-Car bmw = new ("BMW X5", 220);
-Catalog<Car> cars = new (initArray);
-
-cars.Add(new ("Mazda RX7", 256));
-cars.Add(bmw);
-cars.AddRange(range);
-
-Console.WriteLine("Collection after adding new cars:" + Environment.NewLine);
-foreach (var car in cars)
-{
-    Console.WriteLine("Name: " + car.Name + ", MaxSpeed: " + car.MaxSpeed);
-}
-
-Console.WriteLine(Environment.NewLine + "Collection after deleting сars and sorting by maximum speed:" + Environment.NewLine);
-
-cars.Remove(bmw);
-cars.RemoveAt(2);
-cars.Sort();
-
-foreach (var car in cars)
-{
-    Console.WriteLine("Name: " + car.Name + ", MaxSpeed: " + car.MaxSpeed);
-}
+Starter.Run();
