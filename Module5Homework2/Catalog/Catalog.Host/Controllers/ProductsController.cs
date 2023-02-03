1545174e8ecc05b9ec595286f6eb77f9227057c6
Serhii-Myroshnychenko <serzh.miroshnichenko.122@gmail.com>
2023-02-03 15:27:28 +0200

    Added ProductsContreoller file

---
+++ Module5Homework2/Catalog/Catalog.Host/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Host.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly Product[] _products = new[]
+        {
+            new Product() { Id = 1, Name = "Car", Price = 1200},
+            new Product() { Id = 2, Name = "Bike", Price = 750},
+            new Product() { Id = 1, Name = "Spaceship", Price = 3500}
+        };
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                return Ok(_products);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Module5Homework2/Catalog/Catalog.Host/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Host.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Price { get; set; }
+    }
+}
